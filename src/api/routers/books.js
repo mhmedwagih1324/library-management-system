d7d32645330b0294e959d0d35e5202e18e7026a4
Mohamed Wagih <mhmedwagih1324@gmail.com>
2024-10-22 10:51:42 +0300

    add tryCatch logic

--- src/api/routers/books.js
+++ src/api/routers/books.js
@@ -2,9 +2,14 @@ import { Router } from "express";
 import BooksController from "../controllers/books.js";
 import { validate } from "../../common/middleware/validate.js";
 import BooksValidation from "../validations/books.js";
+import tryCatch from "../../common/utils/tryCatch.js";
 
 const router = new Router();
 
-router.get("/", validate(BooksValidation.listBooks), BooksController.listBooks);
+router.get(
+  "/",
+  validate(BooksValidation.listBooks),
+  tryCatch(BooksController.listBooks)
+);
 
 export default router;
