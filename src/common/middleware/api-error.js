b0b57f98388969d5d44ce8fe467abcdaecf18e56
Mohamed Wagih <mhmedwagih1324@gmail.com>
2024-10-23 21:57:54 +0300

    remove error-code

--- src/common/middleware/api-error.js
+++ src/common/middleware/api-error.js
@@ -3,11 +3,10 @@ import httpStatus from "http-status";
 const { INTERNAL_SERVER_ERROR } = httpStatus;
 
 class APIError extends Error {
-  constructor({ errorCode, message, status = INTERNAL_SERVER_ERROR }) {
+  constructor({ message, status = INTERNAL_SERVER_ERROR }) {
     super(message);
     this.name = "APIError";
     this.status = status;
-    this.errorCode = errorCode;
   }
 }
 
