c78ac7c6c75545d9dc8752c9722060dbb67a2a90
Mohamed Wagih <mhmedwagih1324@gmail.com>
2024-10-24 10:27:15 +0300

    Add authorization packages

--- src/config/app.js
+++ src/config/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import router from "./router.js";
 import docsRouter from "./docs-router.js";
 import errorHandler from "../common/middleware/error-handler.js";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(docsRouter);
 
