a654470571ee99a1b22cbfd6cd0294de425bf508
Mohamed Wagih <mhmedwagih1324@gmail.com>
2024-10-25 14:20:57 +0300

    add authentication middleware to `registerBorrower` endpoint

--- src/api/routers/users.js
+++ src/api/routers/users.js
@@ -3,11 +3,13 @@ import { validate } from "../../common/middleware/validate.js";
 import UsersValidation from "../validations/users.js";
 import tryCatch from "../../common/utils/tryCatch.js";
 import UsersController from "../controllers/users.js";
+import { authenticate } from "../../common/middleware/authenticate.js";
 
 const router = new Router();
 
 router.post(
   "/register-borrower",
+  authenticate,
   validate(UsersValidation.registerBorrower),
   tryCatch(UsersController.registerBorrower)
 );
