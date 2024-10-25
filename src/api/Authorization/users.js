5fb8c3289d95f8cd61947d897f4b5624244d20eb
Mohamed Wagih <mhmedwagih1324@gmail.com>
2024-10-25 14:39:25 +0300

    Add authorization to `registerBorrower` endpoint

--- src/api/Authorization/users.js
+++ src/api/Authorization/users.js
@@ -1,16 +1,9 @@
-import { ADMIN } from "../constants.js";
-import User from "../models/users";
+import { ADMIN } from "../constants/users.js";
 
-/**
- * Checks if the user can register a borrower or not
- *
- * @param {Object} Args
- * @param {String} Args.callerId
- *
- * @returns {Promise<Boolean>}
- */
-export const canRegisterBorrower = async ({ callerId }) => {
-  const admin = await User.findOne({ id: callerId });
-
-  return admin.role === ADMIN;
+const UsersAuthorization = {
+  registerBorrower: {
+    roles: [ADMIN],
+  },
 };
+
+export default UsersAuthorization;
--- src/api/routers/users.js
+++ src/api/routers/users.js
@@ -4,12 +4,15 @@ import UsersValidation from "../validations/users.js";
 import tryCatch from "../../common/utils/tryCatch.js";
 import UsersController from "../controllers/users.js";
 import { authenticate } from "../../common/middleware/authenticate.js";
+import { authorize } from "../../common/middleware/authorization.js";
+import UsersAuthorization from "../Authorization/users.js";
 
 const router = new Router();
 
 router.post(
   "/register-borrower",
   authenticate,
+  authorize(UsersAuthorization.registerBorrower),
   validate(UsersValidation.registerBorrower),
   tryCatch(UsersController.registerBorrower)
 );
